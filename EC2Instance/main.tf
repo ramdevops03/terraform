ac989d88243efcafb6f683ed912f2e86f228f052
buraashok <46172238+buraashok@users.noreply.github.com>
2022-07-26 15:25:28 -0400

    sample

--- EC2Instance/main.tf
+++ EC2Instance/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "project-iac" {
-  ami           = "ami-2757f631"
+  ami           = "ami-0cff7528ff583bf9a"
   instance_type = "t2.micro"
   subnet_id = "subnet-dcc1b5ba"
   key_name = "april2022"
