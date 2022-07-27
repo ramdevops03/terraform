6366f7f9588aa4e4ed34fd82865795abb6f3c21a
buraashok <46172238+buraashok@users.noreply.github.com>
2022-07-27 18:18:20 -0400

    sample

--- EC2Instance/main.tf
+++ EC2Instance/main.tf
@@ -4,13 +4,13 @@ provider "aws" {
   secret_key = "NM/ZigLDcGsyrurgra/skBpXMedLahNaEd4VFdiP"
 }
 
-resource "aws_instance" "project-iac" {
+resource "aws_instance" "ec2instance" {
   ami           = "ami-0cff7528ff583bf9a"
   instance_type = "t2.micro"
   subnet_id = "subnet-dcc1b5ba"
   key_name = "april2022"
   tags = {
-    Name ="SERVER01"
+    Name ="SERVER10"
     Environment = "DEV"
     OS = "Linux"
     Managed = "IAC"
